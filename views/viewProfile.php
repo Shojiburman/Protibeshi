fecac9e8d679ba00cf12c3fd08386062c98044ef
unknown <shojiburman@gmail.com>
2020-09-14 01:49:07 +0600

    browse users profile

--- views/viewProfile.php
+++ views/viewProfile.php
@@ -2,53 +2,118 @@
 	session_start();
 	include '../php/session.php';
 	require_once('../db/db.php');
+	$conn = dbConnection();	
+	if(!$conn){	
+		echo "DB connection error";	
+	}
 
 	if(isset($_GET["uid"])){	
 		$userid = $_GET["uid"];
-		$conn = dbConnection();	
-		if(!$conn){	
-			echo "DB connection error";	
-		}	
-		$sql = "SELECT u.us_id, s.name, u.price, u.details, c.name AS cname, u.u_id	, usr.name AS username
-				FROM us_services u	
-				INNER JOIN services s
-				ON s.s_id = u.s_id	
-				INNER JOIN catagory c	
-				ON s.c_id = c.c_id
-                INNER JOIN users usr
-				ON u.u_id = usr.u_id
-				AND u.u_id = '$userid'
-                AND s.flag = '0';";	
 
-		if (($result = $conn->query($sql)) !== FALSE){	
+		$sqlName = "SELECT * FROM users where u_id = '$userid'";
+		if (($result = $conn->query($sqlName)) !== FALSE){	
 	        while($row = $result->fetch_assoc()){	
-				$name = $row['u_id'];	
-				$sname = $row['name'];	
-				$price = $row['price'];	
-				$details = $row['details'];	
-				$catagory = $row['cname'];	
-				$u_id = $row['u_id'];	
-			}	
-		}	
-
-		$sql = "SELECT name from users WHERE u_id = '$u_id'";	
-		if (($result = $conn->query($sql)) !== FALSE){	
-	        while($row = $result->fetch_assoc()){	
-				$name = $row['name'];	
-			}	
+				$name = $row['name'];
+				$bio = $row['bio'];
+				$email = $row['email'];
+				$work = $row['work'];
+				$dob = $row['dob'];
+				$pnumber = $row['pnumber'];	
+			}
 		}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?php echo ucwords($name); ?> Profile</title>
+	<link rel="stylesheet" type="text/css" href="../css/body.css">
+</head>
+<body>
+	<?php
+        if(isset($_SESSION['id']) || isset($_COOKIE['remember'])){
+            if($_SESSION['uType'] == '0'){
+                include '../views/sellerNav.html';
+            } else if($_SESSION['uType'] == '1'){
+                include '../views/buyerNav.html';
+            } else if($_SESSION['uType'] == '2'){
+                include '../views/dealerNav.html';
+            } else if($_SESSION['uType'] == '3'){
+                include '../views/adminNav.html';
+            }
+            
+        } else {
+            include '../views/nav.html';
+        }
+    ?>
 
+	<div id="view-profile">
+		<div class="section">
+			<div class="view-profile-content">
+				<div>
+		            <div id="profilePic">
+		                <img src="<?php echo $c_pic; ?>">
+		            </div>
+		            <div id="addBtn"><img src="add.svg"></div>
+		            <h3>Full Name</h3>
+		            <p class="profile-about-info"><?php echo strtoupper($c_name);?></p>
+		            <h3>Contuct info</h3>
+		            <p class="profile-about-info"><?php echo ($c_email);?></p>
+		            <p class="profile-about-info"><?php echo ($pnumber);?></p>
+		            <h3>Profession</h3>
+		            <p class="profile-about-info"><?php echo ($work);?></p>
+		            <h3>Date of Birth</h3>
+		            <p class="profile-about-info"><?php echo ($dob);?></p>
+		            <h3>About</h3>
+		            <p class="profile-about-info"><?php echo ($bio);?></p>
+		        </div>
+			</div>
+		</div>
+		<div class="section">
+			<div class="view-profile-content">
+				<?php
+				$sql = "SELECT u.us_id, s.name, u.price, u.details, c.name AS cname, u.u_id, usr.name AS username, usr.bio, usr.email, usr.work, usr.dob, usr.pnumber
+						FROM us_services u	
+						INNER JOIN services s
+						ON s.s_id = u.s_id	
+						INNER JOIN catagory c	
+						ON s.c_id = c.c_id
+		                INNER JOIN users usr
+						ON u.u_id = usr.u_id
+						AND u.u_id = '$userid'
+		                AND s.flag = '0';";
+
+				if (($result = $conn->query($sql)) !== FALSE){	
+			        while($row = $result->fetch_assoc()){	
+						$name = $row['username'];	
+						$sname = $row['name'];	
+						$price = $row['price'];	
+						$details = $row['details'];	
+						$catagory = $row['cname'];	
+						$u_id = $row['u_id'];
+						$us_id = $row['us_id'];
+				?>
+				<div class="see-more-service cursor" data-id="">
+					<h1><?php echo $sname ?></h1>
+					<div>
+						<p class="sub-title"><?php echo $name ?></p>
+						<p class="sub-title"><?php echo $catagory ?></p>
+						<p class="sub-title">৳ price</p>
+					</div>
+					<p><?php echo $details ?></p>
+				</div>
+<?php
+			}
+		}
+?>		</div>
+	</div>
+</div>
+<button class="btn" onclick="back()">Back</button>
+<?php	
 	} else {
 		header('Location: ' . $_SERVER['HTTP_REFERER']);
 	}
 
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-	<title><?php echo $userid; ?> Profile</title>
-</head>
-<body>
-
+	<script type="text/javascript" src="../js/script.js"></script>
 </body>
 </html>
